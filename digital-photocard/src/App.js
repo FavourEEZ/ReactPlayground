c3a17fda6e0bba87b346a501ebd185fc55da7c46
FavourEEZ <favouromoragbon2002@gmail.com>
2022-07-13 21:12:30 +0100

    minor styling

--- digital-photocard/src/App.js
+++ digital-photocard/src/App.js
@@ -1,18 +1,19 @@
 import photo from './profilePicture.jpg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <img src={photo} className="App-logo" alt="logo" />
       <div className="div-info">
-        <h3>Favour Omoragbon</h3>
-        <p id="role"> Full Stack Engineer </p>
+        <h1>Favour Omoragbon</h1>
+        <h3 id="role"> Full Stack Engineer </h3>
         <a href="https://reactjs.org" className="App-link">
           favour.ai
         </a>
-
       </div>
+      <Button variant='contained' size='large'> Linkedin </Button> {/**style={{backgroundColor: "white"}} />**/}
 
     </div>
   );
