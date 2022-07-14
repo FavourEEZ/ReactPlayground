08accded271f93e4074c9b6ecfdc0e5d622f8bf7
FavourEEZ <favouromoragbon2002@gmail.com>
2022-07-14 08:50:15 +0100

    Fix footer spacing issue

--- digital-photocard/src/components/footer.js
+++ digital-photocard/src/components/footer.js
@@ -3,11 +3,13 @@ import Button from '@mui/material/Button';
 
 export default function Footer(){
     return (
+    <div className="App">
        <div className="icon-holder div-info">
             <Button size="small">twitter</Button>
             <Button size="small">Facebook</Button>
             <Button size="small">Instagram</Button>
             <Button size="small">Github</Button>
        </div>
+    </div>
     )
 }
