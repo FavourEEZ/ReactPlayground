b40b770e22655d4721c8886b56b6a256f83ff703
FavourEEZ <favouromoragbon2002@gmail.com>
2022-07-23 22:56:55 +0100

    Skelenton structure

---
+++ travel-journal/src/components/Content.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+export default function Content(){
+    return(
+        <div>
+            <h1>Sydney</h1>
+        </div>
+    )
+}
---
+++ travel-journal/src/components/Header.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+export default function Header(){
+    return(
+        <div>
+            <h1>my travel ideas</h1>
+        </div>
+    )
+}
