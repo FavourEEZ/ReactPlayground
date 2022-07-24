d47bb3d2a1827e184090e32f8fe9aba48429ef51
FavourEEZ <favouromoragbon2002@gmail.com>
2022-07-25 00:04:49 +0100

    Bolster dummy data

--- travel-journal/src/components/Content.js
+++ travel-journal/src/components/Content.js
@@ -4,6 +4,8 @@ export default function Content(props){
     return(
         <div>
             <h1>{props.place}</h1>
+            <p>While in {props.country} on {props.date}</p>
+            <a href={props.googleMapsLink}>Link to the location</a>
             {/* <h1>China</h1>
             <h1>Monaco</h1>
             <h1>Costa Rica</h1> */}
--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -3,7 +3,7 @@ export default [
         img: "",
         place: "London",
         country: "England",
-        googleMapsLink: "",
+        googleMapsLink: "https://goo.gl/maps/t62RJGuweDAoBF2J9",
         date: "25th July - 29th July",
         description: ""
     },
@@ -11,16 +11,16 @@ export default [
         img: "",
         place: "Bangkok",
         country: "Thailand",
-        googleMapsLink: "",
+        googleMapsLink: "https://goo.gl/maps/h5J3AFNZozE3fHZX6",
         date: "2nd August 2023",
         description: ""
     },
     {
         img: "",
         place: "Monaco",
-        country: "",
-        googleMapsLink: "",
-        date: "",
+        country: "Monaco",
+        googleMapsLink: "https://goo.gl/maps/J9YgatwrQ4KnBDJE6",
+        date: "14th Febuary 2024",
         description: ""
     }
 
