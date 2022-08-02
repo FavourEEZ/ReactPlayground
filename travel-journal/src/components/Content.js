875c625812c620e419b0b256e7be54b801c0bd5e
FavourEEZ <favouromoragbon2002@gmail.com>
2022-08-02 23:42:11 +0100

    Initial commit for CRUD

--- travel-journal/src/components/Content.js
+++ travel-journal/src/components/Content.js
@@ -4,6 +4,8 @@ export default function Content(props){
     return(
         <div>
             <h1>{props.place}</h1>
+            <img src={props.img} width="400px"  />
+
             <p>While in {props.country} on {props.date}</p>
             <a href={props.googleMapsLink}>Link to the location</a>
             {/* <h1>China</h1>
--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -1,14 +1,18 @@
+import bangkok from "./assets/Bangkok.jpg";
+import london from "./assets/London.jpg";
+import monaco from "./assets/Monaco.jpg"
+
 export default [
     {
-        img: "",
+        img: london,
         place: "London",
         country: "England",
         googleMapsLink: "https://goo.gl/maps/t62RJGuweDAoBF2J9",
         date: "25th July - 29th July",
-        description: ""
+        description: `A wonderful time in London with the best people`
     },
     {
-        img: "",
+        img: bangkok,
         place: "Bangkok",
         country: "Thailand",
         googleMapsLink: "https://goo.gl/maps/h5J3AFNZozE3fHZX6",
@@ -16,7 +20,7 @@ export default [
         description: ""
     },
     {
-        img: "",
+        img: monaco,
         place: "Monaco",
         country: "Monaco",
         googleMapsLink: "https://goo.gl/maps/J9YgatwrQ4KnBDJE6",
