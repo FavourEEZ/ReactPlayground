ffc32009a19edbc35dcdb6d95ce5a33d5d4f9fc7
FavourEEZ <favouromoragbon2002@gmail.com>
2022-08-14 19:28:16 -0400

    No days off!!!!

--- travel-journal/src/components/Content.js
+++ travel-journal/src/components/Content.js
@@ -1,12 +1,13 @@
 import '../App.css';
 
 export default function Content(props){
+
     return(
         <div>
             <h1>{props.place}</h1>
             <img src={props.img} width="400px"  />
 
-            <p>While in {props.country} on {props.date}</p>
+            <p>In {props.country} on {props.date}</p>
             <a href={props.googleMapsLink}>Link to the location</a>
 
         </div>
